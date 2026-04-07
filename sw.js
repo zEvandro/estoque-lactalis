0f48e052540e0ebf03476ed37dc32062af179db0
Evandro R Silva <116104353+zEvandro@users.noreply.github.com>
2026-04-07 15:13:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ═══════════════════════════════════════════════
 // ESTOQUE CONTAGEM — Service Worker v4.1
 // ═══════════════════════════════════════════════
-const CACHE_NAME = 'estoque-v18';
+const CACHE_NAME = 'estoque-v19';
 
 // Recursos para cachear na instalação
 const PRECACHE = [
